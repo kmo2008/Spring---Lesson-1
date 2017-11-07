41dae23560368426a14f382e289f089a9217704d
kmo2008 <kamilostry20@gmail.com>
2017-11-07 16:02:17 +0100

    Show, add, modify, remove pizza

--- src/main/java/com/example/demo/Summary.java
+++ src/main/java/com/example/demo/Summary.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Summary {
@@ -12,22 +9,22 @@ public class Summary {
     @Autowired
     private Order order;
 
-    @RequestMapping("/showPizza")
+    @RequestMapping(value = "/showPizza", method = RequestMethod.POST)
     public String showPizza(@RequestParam int id) {
         return order.showPizza(id);
     }
 
-    @RequestMapping("/addPizza")
-    public String addPizza(@RequestParam int id,@RequestParam String name,@RequestParam float prize) {
+    @RequestMapping(value = "/addPizza", method = RequestMethod.POST)
+    public String addPizza(@RequestParam int id,@RequestParam  String name,@RequestParam  float prize) {
         return order.addPizza(id, name, prize);
     }
 
-    @RequestMapping("/modifyPizza")
-    public String modifyPizza(@RequestParam int id,@RequestParam String name,@RequestParam float prize) {
+    @RequestMapping(value = "/modifyPizza", method = RequestMethod.POST)
+    public String modifyPizza(@RequestParam int id,@RequestParam  String name,@RequestParam  float prize) {
         return order.modifyPizza(id, name, prize);
     }
 
-    @RequestMapping("/delPizza")
+    @RequestMapping(value = "/delPizza", method = RequestMethod.POST)
     public String delPizza(@RequestParam int id) {
         return order.delPizza(id);
     }
